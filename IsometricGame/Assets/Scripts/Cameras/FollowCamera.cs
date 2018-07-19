269b81b2d10fdc78dd2645ccc07f4061327fe469
Gary <garytmulhall@gmail.com>
2018-07-19 21:43:06 +0100

    Rotatable player camera
    -Added a camera which can be rotated around the player based on user input.

--- IsometricGame/Assets/Scripts/Cameras/FollowCamera.cs
+++ IsometricGame/Assets/Scripts/Cameras/FollowCamera.cs
@@ -6,24 +6,9 @@ public class FollowCamera : MonoBehaviour
     public Transform target;
     /*A float to used to adjust the smoothness of the camera snapping to target*/
     public float smoothSpeed = 0.25f;
-    /*A vector for offsetting the camera*/
-    public Vector3 offset;
-    /*A boolean to check if the camera should rotate or not*/
-    public bool rotate;
-    /*A float for the rotation speed*/
-    public float rotationSpeed;
-    /*Using a late update makes the camera motion smoother*/
-    void Start()
-    {
-    }
+
     void LateUpdate()
     {
-        if (rotate == true)
-        {
-            Quaternion cameraTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
-            offset = cameraTurnAngle * offset;
-            transform.position = target.position + offset;
-        }
-        transform.position = target.position + offset;
+        transform.position = target.position;
     }
 }
---
+++ IsometricGame/Assets/Scripts/Cameras/OrbitCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{ 
+    public Transform cameraFocus;
+    public float rotateSpeed;
+
+    void LateUpdate()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.RotateAround(cameraFocus.position, Vector3.up, rotateSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.RotateAround(cameraFocus.position, -Vector3.up, rotateSpeed * Time.deltaTime);
+        }
+    }
+}
